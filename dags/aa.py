31f677daeb52f259d2feadad99e09812f335583b
yannoushka74 <yann.trieu@gmail.com>
2025-06-11 14:52:19 +0200

    kk

---
+++ dags/aa.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("✅ DAG synchronisé depuis Git avec succès !")
+
+with DAG(
+    dag_id="test_git_sync_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,  # Pas d'exécution automatique
+    catchup=False,
+    tags=["test", "git-sync"],
+) as dag:
+    
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello,
+    )
